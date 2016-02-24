bc86498431756030d3d2c195ad83024d2081b7de
gevans6 <grant.evans300@gmail.com>
2016-02-23 21:44:30 -0800

    Grant's updated changes to chart 2

--- scripts/chart2.r
+++ scripts/chart2.r
@@ -1,26 +1,22 @@
-library(dplyr)
-library(plotly)
 # A function that takes a dataset as a parameter and returns a visualization chart
-chart_two <- function(data) {
+chart_two <- function(dataset) {
   
   # Creating a summary table of the totals for the different types of people
-  summary_tab <- data %>% 
-    summarise("Number of Mac Users" = sum(
-                data$What.orperating.system.do.you.typically.use. == "Mac"),
-              "Number of Windows users" = sum(
-                data$What.orperating.system.do.you.typically.use. == "Windows"), 
-              "Number of Windows Users interested in Informatics" = sum((data$What.orperating.system.do.you.typically.use. == "Windows")) &
-                (data$Are.you.interested.in.applying.to.the.Informatics.major. == "Yes"))
-              "Number of Mac Users interested in Informatics" = sum((data$What.orperating.system.do.you.typically.use. == "Mac") & 
-                (data$Are.you.interested.in.applying.to.the.Informatics.major. == "Yes"))
-  
-  # Set up axis names
-  xaxis <- list(title = 'Type of Person')
+  summary_tab <- dataset %>% 
+    summarise("Number of Mac Users" = sum(operating_system == "Mac"),
+              "Number of Windows users" = sum(operating_system == "Windows"), 
+              "Number of Mac Users interested in Info" = 
+                sum((operating_system == "Mac") & (info_interest == "Yes")),
+              "Number of Windows Users interested in Info" = 
+                sum((operating_system == "Windows") & (info_interest == "Yes"))
+    )
+              
+  # Set up axis names and fonts
+  xaxis <- list(title = 'Type of Person', tickangle = 8, tickfont = list(size = 9))
   yaxis <- list(title = 'Number of People')
   
   # Create a bar graph 
   plot_ly(summary_tab, type = 'bar', x = names(summary_tab), 
           y = as.numeric(summary_tab[1,])) %>% 
     layout(title = 'Are PC users or Mac users more interested in Informatics?', xaxis=xaxis, yaxis=yaxis)
-  
 }
